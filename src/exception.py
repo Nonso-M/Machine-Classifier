5cb53fbfcb0611c69d3ff2b57e984e30ecb2d9d9
Nonso Muolokwu <muolokwunonso@gmail.com>
2023-03-21 19:20:59 +0100

    Updated the data preprcessing files and utilsfiles

--- src/exception.py
+++ src/exception.py
@@ -1,4 +1,5 @@
 import sys
+from src.logger import logging
 
 
 def error_message_detail(error, error_detail: sys):
@@ -9,7 +10,7 @@ def error_message_detail(error, error_detail: sys):
     return error_message
 
 
-class CustomeException(Exception):
+class CustomException(Exception):
     def __init__(self, error_message, error_detail):
         super().__init__(error_message)
         self.error_message = error_message_detail(
@@ -18,3 +19,11 @@ class CustomeException(Exception):
 
     def __str__(self):
         return self.error_message
+
+
+if __name__ == "__main__":
+    try:
+        a = 1 / 0
+    except Exception as e:
+        logging.info("Divide by zero error")
+        raise CustomException(e, sys)
